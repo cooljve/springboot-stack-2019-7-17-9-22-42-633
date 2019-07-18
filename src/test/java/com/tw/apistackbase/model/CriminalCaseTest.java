3f9294856acc337325b716b0bf87d046cc5dda95
LIUJO5 <joi.liu@oocl.com>
2019-07-18 13:49:11 +0800

    modify h2 password

--- src/test/java/com/tw/apistackbase/model/CriminalCaseTest.java
+++ src/test/java/com/tw/apistackbase/model/CriminalCaseTest.java
@@ -13,8 +13,6 @@ import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.in;
-import static org.junit.Assert.assertNotNull;
 
 @DataJpaTest
 @ExtendWith(SpringExtension.class)
